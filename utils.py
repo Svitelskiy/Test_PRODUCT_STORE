3e20bae65b20aa93b7fabbca6f7c8ae524073adf
Svitelskiy <85756206+Svitelskiy@users.noreply.github.com>
2021-09-15 11:33:07 +0300

    updated argument name

--- utils.py
+++ utils.py
@@ -9,14 +9,14 @@ def wait_for_alert_and_accept(browser, time_to_wait):
     alert.accept()
 
 
-def wait_for_element_be_located(browser, time_to_wait, By_element, element_id):
-    if By_element == "CLASS_NAME":
+def wait_for_element_be_located(browser, time_to_wait, by_element, element_id):
+    if by_element == "CLASS_NAME":
         return WebDriverWait(browser, time_to_wait).until(EC.visibility_of_element_located((By.CLASS_NAME, element_id)))
-    elif By_element == "XPATH":
+    elif by_element == "XPATH":
         return WebDriverWait(browser, time_to_wait).until(EC.visibility_of_element_located((By.XPATH, element_id)))
-    elif By_element == "ID":
+    elif by_element == "ID":
         return WebDriverWait(browser, time_to_wait).until(EC.visibility_of_element_located((By.ID, element_id)))
-    elif By_element == "CSS_SELECTOR":
+    elif by_element == "CSS_SELECTOR":
         return WebDriverWait(browser, time_to_wait).until(EC.visibility_of_element_located((By.CSS_SELECTOR, element_id)))
 
 
